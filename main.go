bf391f083bc4904b782ee344d6a434d304015a62
Dhamith Hewamullage <hewamullage123@gmail.com>
2020-11-01 04:18:18 +0000

    cleanup test code

--- main.go
+++ main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"symon/config"
-	"symon/monitor"
+	// "encoding/json"
+	// "fmt"
+	// "symon/config"
+	// "symon/monitor"
 	"symon/server"
 )
 
 func main() {
-	conf := config.Config{MonitorInterval: 5, LogFileEnabled: true, LogFilePath: "/path/to", DBPath: "/path/to"}
-
-	byteArray, err := json.Marshal(conf)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("%+v\n", conf)
-	fmt.Println(string(byteArray))
-
-	system := monitor.GetSystem()
-	memory := monitor.GetMemory()
-
-	fmt.Printf("%+v\n", system)
-	fmt.Printf("%+v\n", memory)
+	// conf := config.Config{MonitorInterval: 5, LogFileEnabled: true, LogFilePath: "/path/to", DBPath: "/path/to"}
 	server.Run()
 }
