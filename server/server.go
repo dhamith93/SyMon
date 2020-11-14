909be55c9ea8c86999b0974bb3efadafe781aee6
Dhamith Hewamullage <hewamullage123@gmail.com>
2020-11-14 15:40:43 +0000

    API auth

--- server/server.go
+++ server/server.go
@@ -34,49 +34,92 @@ func Run(port string) {
 	handleRequests(port)
 }
 
+func isAuthorized(w http.ResponseWriter, r *http.Request) bool {
+	if len(r.Header["Key"]) != 0 && r.Header["Key"][0] == util.GetKey() {
+		return true
+	}
+	w.WriteHeader(http.StatusUnauthorized)
+	json.NewEncoder(w).Encode("")
+	ip, _ := util.GetIncomingIPAddr(r)
+	util.Log("warn", "Unauthorized request from "+ip)
+	return false
+}
+
 func returnSystem(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	system := monitor.GetSystem()
 	json.NewEncoder(w).Encode(system)
 }
 
 func returnMemory(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	memory := monitor.GetMemory()
 	json.NewEncoder(w).Encode(memory)
 }
 
 func returnSwap(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	swap := monitor.GetSwap()
 	json.NewEncoder(w).Encode(swap)
 }
 
 func returnDisks(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	disks := monitor.GetDisks()
 	json.NewEncoder(w).Encode(disks)
 }
 
 func returnProc(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	proc := monitor.GetProcessor()
 	json.NewEncoder(w).Encode(proc)
 }
 
 func returnNetwork(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	network := monitor.GetNetwork()
 	json.NewEncoder(w).Encode(network)
 }
 
 func returnMemUsage(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	memUsage := monitor.GetProcessesSortedByMem()
 	json.NewEncoder(w).Encode(memUsage)
 }
 
 func returnCPUUsage(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(w, r) {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	cpuUsage := monitor.GetProcessesSortedByCPU()
 	json.NewEncoder(w).Encode(cpuUsage)
---
+++ util/keygen.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"bufio"
+	"crypto/rand"
+	b64 "encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func GetKey() string {
+	if _, err := os.Stat("key"); err == nil {
+		b, err := ioutil.ReadFile("key")
+		if err != nil {
+			log.Println(err.Error())
+			panic(err)
+		}
+		return string(b)
+	}
+
+	key := keyGen()
+	file, err := os.Create("key")
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	w := bufio.NewWriter(file)
+	_, err = fmt.Fprintf(w, "%v", key)
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	w.Flush()
+
+	return key
+}
+
+func keyGen() string {
+	key := make([]byte, 64)
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err)
+	}
+	return b64.StdEncoding.EncodeToString(key)
+}
