62b3a7b8dff4bbf7b6e8700440db3e07b0a8eb81
Dhamith Hewamullage <hewamullage123@gmail.com>
2020-11-14 09:45:04 +0000

    Config handler

---
+++ util/config_handler.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Config struct with config
+type Config struct {
+	SSLEnabled             bool
+	SSLCertFilePath        string
+	SSLKeyFilePath         string
+	LogFileEnabled         bool
+	LogFilePath            string
+	MonitoringInterval     uint8
+	SQLiteDBLoggingEnabled bool
+	SQLiteDBPath           string
+}
+
+// GetConfig return config struct
+func GetConfig() Config {
+	file, err := ioutil.ReadFile("config.json")
+	config := Config{}
+
+	if err != nil {
+		return config
+	}
+
+	_ = json.Unmarshal([]byte(file), &config)
+
+	return config
+}
