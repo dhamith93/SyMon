87dc54ffb89fcf0041b7732cc60b9123b9aaf967
Dhamith Hewamullage <hewamullage123@gmail.com>
2021-08-21 13:59:29 +0530

    Send GET method

--- internal/send/send.go
+++ internal/send/send.go
@@ -2,7 +2,6 @@ package send
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -10,32 +9,49 @@ import (
 	"github.com/dhamith93/SyMon/internal/logger"
 )
 
-func SendPost(url string, json string) bool {
+func SendPost(url string, json string) (bool, int, string) {
 	var jsonStr = []byte(json)
 	token, err := auth.GenerateJWT()
 	if err != nil {
 		logger.Log("Error", err.Error())
-		return false
+		return false, -1, ""
 	}
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Token", token)
 	req.Header.Set("Content-Type", "application/json")
 
+	return send(req, url)
+}
+
+func SendGet(url string) (bool, int, string) {
+	token, err := auth.GenerateJWT()
+	if err != nil {
+		logger.Log("Error", err.Error())
+		return false, -1, ""
+	}
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Token", token)
+
+	return send(req, url)
+}
+
+func send(req *http.Request, url string) (bool, int, string) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		logger.Log("Error", err.Error())
-		return false
+		return false, -1, ""
 	}
 	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	str := string(body)
 
 	if resp.StatusCode == 200 {
-		return true
+		return true, resp.StatusCode, str
 	}
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
-	logger.Log("Error", url+" - "+resp.Status+" - "+string(body))
-	return false
+	logger.Log("Error", url+" - "+resp.Status+" - "+str)
+	return false, resp.StatusCode, str
 }
