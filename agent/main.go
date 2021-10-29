dfffd6b2deccdab963ba729bb038c9e20393cb7e
Dhamith Hewamullage <hewamullage123@gmail.com>
2021-10-30 01:00:09 +0530

    Initial switch to gRPC agent->collector

--- agent/main.go
+++ agent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -10,9 +11,13 @@ import (
 	"sync"
 	"time"
 
+	"github.com/dhamith93/SyMon/internal/api"
+	"github.com/dhamith93/SyMon/internal/auth"
 	"github.com/dhamith93/SyMon/internal/config"
+	"github.com/dhamith93/SyMon/internal/logger"
 	"github.com/dhamith93/SyMon/internal/monitor"
-	"github.com/dhamith93/SyMon/internal/send"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 func main() {
@@ -37,11 +42,11 @@ func main() {
 	flag.Parse()
 
 	if *initPtr {
-		initAgent(config)
+		initAgent(&config)
 		return
 	} else if *customPtr {
 		if len(name) > 0 && len(value) > 0 && len(unit) > 0 {
-			sendCustomMetric(name, unit, value, config)
+			sendCustomMetric(name, unit, value, &config)
 		} else {
 			fmt.Println("Metric name, unit, and value all required")
 		}
@@ -56,8 +61,8 @@ func main() {
 		for {
 			select {
 			case <-ticker.C:
-				monitorData := monitor.MonitorAsJSON(config)
-				send.SendPost(config.MonitorEndpoint, monitorData)
+				monitorData := monitor.MonitorAsJSON(&config)
+				sendMonitorData(monitorData, &config)
 			case <-quit:
 				ticker.Stop()
 				return
@@ -68,16 +73,33 @@ func main() {
 	fmt.Println("Exiting")
 }
 
-func initAgent(config config.Config) {
-	_, code, response := send.SendGet(config.MonitorEndpoint + "-init?serverId=" + config.ServerId + "&timezone=" + monitor.GetSystem().TimeZone)
-	if code == -1 {
-		fmt.Println("Cannot connect to collector, make sure config.json is correct and collector is running.")
-	} else {
-		fmt.Println(response)
+func initAgent(config *config.Config) {
+	conn, c, ctx, cancel := createClient(config)
+	defer conn.Close()
+	defer cancel()
+	response, err := c.InitAgent(ctx, &api.ServerInfo{
+		ServerId: config.ServerId,
+		Timezone: monitor.GetSystem().TimeZone,
+	})
+	if err != nil {
+		logger.Log("error", "error adding agent: "+err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("%s \n", response.Body)
+}
+
+func sendMonitorData(monitorData string, config *config.Config) {
+	conn, c, ctx, cancel := createClient(config)
+	defer conn.Close()
+	defer cancel()
+	_, err := c.HandleMonitorData(ctx, &api.MonitorData{MonitorData: monitorData})
+	if err != nil {
+		logger.Log("error", "error sending data: "+err.Error())
+		os.Exit(1)
 	}
 }
 
-func sendCustomMetric(name string, unit string, value string, config config.Config) {
+func sendCustomMetric(name string, unit string, value string, config *config.Config) {
 	customMetric := monitor.CustomMetric{
 		Name:     name,
 		Unit:     unit,
@@ -90,5 +112,33 @@ func sendCustomMetric(name string, unit string, value string, config config.Conf
 		fmt.Println(err.Error())
 		return
 	}
-	send.SendPost(config.MonitorEndpoint+"-custom", string(jsonData))
+	conn, c, ctx, cancel := createClient(config)
+	defer conn.Close()
+	defer cancel()
+	_, err = c.HandleCustomMonitorData(ctx, &api.MonitorData{MonitorData: string(jsonData)})
+	if err != nil {
+		logger.Log("error", "error sending custom data: "+err.Error())
+		os.Exit(1)
+	}
+}
+
+func generateToken() string {
+	token, err := auth.GenerateJWT()
+	if err != nil {
+		logger.Log("error", "error generating token: "+err.Error())
+		os.Exit(1)
+	}
+	return token
+}
+
+func createClient(config *config.Config) (*grpc.ClientConn, api.MonitorDataServiceClient, context.Context, context.CancelFunc) {
+	conn, err := grpc.Dial(config.MonitorEndpoint, grpc.WithInsecure())
+	if err != nil {
+		logger.Log("error", "connection error: "+err.Error())
+		os.Exit(1)
+	}
+	c := api.NewMonitorDataServiceClient(conn)
+	token := generateToken()
+	ctx, cancel := context.WithTimeout(metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{"jwt": token})), time.Second*1)
+	return conn, c, ctx, cancel
 }
