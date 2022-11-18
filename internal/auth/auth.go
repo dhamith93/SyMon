d6cf0d0e32e15e8b3c0b6da872ea6e7173ff05e9
Dhamith Hewamullage <hewamullage123@gmail.com>
2022-11-18 14:04:18 +0530

    moving the `key` to env-var from file

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,57 +1,22 @@
 package auth
 
 import (
-	"bufio"
 	"crypto/rand"
 	b64 "encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
-	"strings"
 	"time"
 
-	"github.com/dhamith93/SyMon/internal/command"
 	"github.com/dhamith93/SyMon/internal/logger"
 	"github.com/golang-jwt/jwt"
 )
-
-func CheckAuth(endpoint func(w http.ResponseWriter, r *http.Request)) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["Token"] != nil {
-			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("error with method")
-				}
-				return []byte(GetKey()), nil
-			})
-
-			if err != nil {
-				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode("Unauthorized")
-				logger.Log("Auth Error", err.Error())
-			}
-
-			if token.Valid {
-				endpoint(w, r)
-			}
-		} else {
-			w.WriteHeader(http.StatusUnauthorized)
-			json.NewEncoder(w).Encode("Unauthorized")
-			ip, _ := command.GetIncomingIPAddr(r)
-			logger.Log("Auth Error", "Unauthorized request from "+ip)
-		}
-	})
-}
 
 func ValidToken(token string) bool {
 	t, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("error with method")
 		}
-		return []byte(GetKey()), nil
+		return []byte(GetKey(false)), nil
 	})
 	if err != nil {
 		logger.Log("Auth Error", err.Error())
@@ -68,7 +33,7 @@ func GenerateJWT() (string, error) {
 	claims["client"] = "test-client"
 	claims["exp"] = time.Now().Add(time.Minute).Unix()
 
-	tokenString, err := token.SignedString([]byte(GetKey()))
+	tokenString, err := token.SignedString([]byte(GetKey(false)))
 
 	if err != nil {
 		return "", err
@@ -77,30 +42,20 @@ func GenerateJWT() (string, error) {
 	return tokenString, nil
 }
 
-// GetKey returns key from file if exist or generate key
-func GetKey() string {
-	if _, err := os.Stat("key"); err == nil {
-		b, err := ioutil.ReadFile("key")
-		if err != nil {
-			log.Println(err.Error())
-			panic(err)
-		}
-		return strings.TrimSpace(string(b))
+func GetKey(generateIfNotFound bool) string {
+	key := os.Getenv("SYMON_KEY")
+
+	if len(key) > 0 {
+		return key
 	}
 
-	key := keyGen()
-	file, err := os.Create("key")
-	if err != nil {
-		log.Println(err.Error())
-		panic(err)
+	if !generateIfNotFound {
+		return ""
 	}
-	w := bufio.NewWriter(file)
-	_, err = fmt.Fprintf(w, "%v", key)
-	if err != nil {
-		log.Println(err.Error())
-		panic(err)
-	}
-	w.Flush()
+
+	key = keyGen()
+	err := os.Setenv("SYMON_KEY", key)
+	logger.Log("ERR", err.Error())
 
 	return key
 }
